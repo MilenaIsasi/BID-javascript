2edee04e62c65ff80b2879377785f7c63223e432
MilenaIsasi <115422599+MilenaIsasi@users.noreply.github.com>
2023-01-06 23:06:23 -0300

    Add files via upload

---
+++ Desestructuración/2.js
@@ -0,0 +1,9 @@
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); //Elon
+console.log(otherName); //undefined 
